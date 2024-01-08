f1000c3782291e45e6c9a125320a9b685a3cf130
40186936 <40186936@UX-N103-13>
2024-01-08 15:11:53 +0000

    Fnished lesson 1, Need to go over some parts in Javascript to regain some knowledge

--- Lesson1/JSDB1.js
+++ Lesson1/JSDB1.js
@@ -2,8 +2,8 @@ var first = 0;
 var second = 0;
 var total = 0;
 
-document.write("Your numbers are: ", first," ", second," and ", total)
-document.write("Setting numbers...");
+document.write("Your numbers are: ", first," ", second," and ", total, "<br>")
+document.write("Setting numbers... <br>");
 first = 10;
 second = 20;
 total = first+second;
