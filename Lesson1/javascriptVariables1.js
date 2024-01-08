8ea9aec932e17db06b4360ce249d2f8014047128
40186936 <40186936@UX-N103-13>
2024-01-08 15:24:41 +0000

    Finished Lesson 1, Need to go over some Javascript to regain some knowledge

---
+++ Lesson1/javascriptVariables1.js
@@ -0,0 +1,14 @@
+var student_name = "Lawand";
+var student_ID = 40186936;
+var english_score = 0;
+var maths_score = 0;
+var science_score = 0;
+
+document.write("Student name: ",student_name,"<br>");
+document.write("Student ID: ",student_ID,"<br>");
+english_score = 100;
+maths_score = 90;
+science_score = 70;
+average_score = (english_score + maths_score + science_score) / 3
+document.write("The average score for student ",student_name,": ",average_score)
+
