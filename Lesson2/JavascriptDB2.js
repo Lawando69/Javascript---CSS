0f6c977fdb55525d446c0033f828b19f7375cb45
40186936 <40186936@UX-N103-13>
2024-01-15 14:18:26 +0000

    Commit 13423412314234

---
+++ Lesson2/JavascriptDB2.js
@@ -0,0 +1,19 @@
+// Javascript Debugging Exercise 2
+
+var name = "";
+var id = "";
+var monthlyWage = 0;
+var yearlyWage = 0;
+
+document.write("Employee Details: ", name," ", id," ", monthlyWage, "</br>");
+document.write("Payrise!</br>");
+
+Name = "Bob"
+
+id = "1211212"
+
+monthlyWage = 500;
+
+yearlyWage = monthlyWage*12;
+
+document.write("Employee Details: ", Name," ", id," ", monthlyWage, "</br>");
